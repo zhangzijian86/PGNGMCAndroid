ebd4845cab4121a5b960c1aa351e1821aa9777fb
zhangzijian86 <zhangzijian86@163.com>
2017-04-06 22:26:40 +0800

    appuser ok

--- NGMCAndroid/src/main/java/com/pg/ngmc/pgngmcandroid/activity/SplashActivity.java
+++ NGMCAndroid/src/main/java/com/pg/ngmc/pgngmcandroid/activity/SplashActivity.java
@@ -16,7 +16,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
         mHandler = new Handler();
         mHandler.postDelayed(toMainActivity, 3000);
 
